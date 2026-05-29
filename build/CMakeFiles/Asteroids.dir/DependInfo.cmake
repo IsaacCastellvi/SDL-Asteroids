81b94f8faad2538aef966355c2edebef7c458ec3
Bryteep <139593534+IsaacCastellvi@users.noreply.github.com>
2026-05-29 16:55:57 +0200

    git chanches

---
+++ build/CMakeFiles/Asteroids.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/isi/Documents/GitHub/SDL-Asteroids/src/GameScene.cpp" "CMakeFiles/Asteroids.dir/src/GameScene.cpp.o" "gcc" "CMakeFiles/Asteroids.dir/src/GameScene.cpp.o.d"
+  "/home/isi/Documents/GitHub/SDL-Asteroids/src/InputManager.cpp" "CMakeFiles/Asteroids.dir/src/InputManager.cpp.o" "gcc" "CMakeFiles/Asteroids.dir/src/InputManager.cpp.o.d"
+  "/home/isi/Documents/GitHub/SDL-Asteroids/src/MenuScene.cpp" "CMakeFiles/Asteroids.dir/src/MenuScene.cpp.o" "gcc" "CMakeFiles/Asteroids.dir/src/MenuScene.cpp.o.d"
+  "/home/isi/Documents/GitHub/SDL-Asteroids/src/asteroid.cpp" "CMakeFiles/Asteroids.dir/src/asteroid.cpp.o" "gcc" "CMakeFiles/Asteroids.dir/src/asteroid.cpp.o.d"
+  "/home/isi/Documents/GitHub/SDL-Asteroids/src/bullet.cpp" "CMakeFiles/Asteroids.dir/src/bullet.cpp.o" "gcc" "CMakeFiles/Asteroids.dir/src/bullet.cpp.o.d"
+  "/home/isi/Documents/GitHub/SDL-Asteroids/src/main.cpp" "CMakeFiles/Asteroids.dir/src/main.cpp.o" "gcc" "CMakeFiles/Asteroids.dir/src/main.cpp.o.d"
+  "/home/isi/Documents/GitHub/SDL-Asteroids/src/scene.cpp" "CMakeFiles/Asteroids.dir/src/scene.cpp.o" "gcc" "CMakeFiles/Asteroids.dir/src/scene.cpp.o.d"
+  "/home/isi/Documents/GitHub/SDL-Asteroids/src/ship.cpp" "CMakeFiles/Asteroids.dir/src/ship.cpp.o" "gcc" "CMakeFiles/Asteroids.dir/src/ship.cpp.o.d"
+  "" "Asteroids" "gcc" "CMakeFiles/Asteroids.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Asteroids.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Asteroids.dir/link.d"
+  "Asteroids"
+  "Asteroids.pdb"
+  "CMakeFiles/Asteroids.dir/src/GameScene.cpp.o"
+  "CMakeFiles/Asteroids.dir/src/GameScene.cpp.o.d"
+  "CMakeFiles/Asteroids.dir/src/InputManager.cpp.o"
+  "CMakeFiles/Asteroids.dir/src/InputManager.cpp.o.d"
+  "CMakeFiles/Asteroids.dir/src/MenuScene.cpp.o"
+  "CMakeFiles/Asteroids.dir/src/MenuScene.cpp.o.d"
+  "CMakeFiles/Asteroids.dir/src/asteroid.cpp.o"
+  "CMakeFiles/Asteroids.dir/src/asteroid.cpp.o.d"
+  "CMakeFiles/Asteroids.dir/src/bullet.cpp.o"
+  "CMakeFiles/Asteroids.dir/src/bullet.cpp.o.d"
+  "CMakeFiles/Asteroids.dir/src/main.cpp.o"
+  "CMakeFiles/Asteroids.dir/src/main.cpp.o.d"
+  "CMakeFiles/Asteroids.dir/src/scene.cpp.o"
+  "CMakeFiles/Asteroids.dir/src/scene.cpp.o.d"
+  "CMakeFiles/Asteroids.dir/src/ship.cpp.o"
+  "CMakeFiles/Asteroids.dir/src/ship.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Asteroids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
